d8c9efa14ec4a813ee306c22bdded318bd2aa11e
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-01-28 03:23:14 +0100

    cv-front | Started setup for deployment file

--- cv-front/src/app/page.tsx
+++ cv-front/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          En procés d'edició de&nbsp;
+          En procés d&apos;edició de&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
         <p > Aquí comença la Cercavila!</p>
@@ -51,7 +51,6 @@ export default function Home() {
                 alt="Logotip de Cercavila"
                 width={510}
                 height={56}
-                align="right"
                 priority
             />
           </a>
@@ -123,7 +122,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Descobreix més sobre l'equip darrere Cercavila.
+            Descobreix més sobre l&apos;equip darrere Cercavila.
           </p>
         </a>
       </div>
