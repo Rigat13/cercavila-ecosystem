3d53f8d0e25756cfbf6e997e2c7ae06f51921448
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-01-29 17:34:41 +0100

    cv-api | Application service createColla added

--- cv-api/src/main/java/cat/cercavila/cvapi/colles/application/service/CreateCollaService.java
+++ cv-api/src/main/java/cat/cercavila/cvapi/colles/application/service/CreateCollaService.java
@@ -2,14 +2,20 @@ package cat.cercavila.cvapi.colles.application.service;
 
 import cat.cercavila.cvapi.colles.application.port.in.CreateColla;
 import cat.cercavila.cvapi.colles.application.port.in.CreateCollaCommand;
+import cat.cercavila.cvapi.colles.application.port.out.StoreCollaPort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCollaService implements CreateColla {
+    private final StoreCollaPort storeCollaPort;
+
+    public CreateCollaService(StoreCollaPort storeCollaPort) {
+        this.storeCollaPort = storeCollaPort;
+    }
 
     @Override
     public void createColla(CreateCollaCommand createCollaCommand) {
-
+        storeCollaPort.storeColla(createCollaCommand);
     }
 
 }
