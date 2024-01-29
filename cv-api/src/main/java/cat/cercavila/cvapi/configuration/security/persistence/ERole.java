d47006dc6578912c031aa9ed625c7c5ae2bdfcac
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-01-29 20:33:55 +0100

    cv-api | Configuration Security Persistence added
    Added:
    ERole
    Role
    UserLab
    UserLabRepository

---
+++ cv-api/src/main/java/cat/cercavila/cvapi/configuration/security/persistence/ERole.java
@@ -0,0 +1,9 @@
+package cat.cercavila.cvapi.configuration.security.persistence;
+
+public enum ERole {
+    ROLE_VISITOR,
+    ROLE_USER,
+    ROLE_CAP_DE_COLLA,
+    ROLE_COLLABORATOR,
+    ROLE_ADMIN
+}
