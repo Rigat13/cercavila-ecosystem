36ee61803565cf6a061096528181d3c101137cfb
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-01-29 19:35:39 +0100

    cv-api | Configuration RestClient web added

---
+++ cv-api/src/main/java/cat/cercavila/cvapi/configuration/restClient/WebClientConfiguration.java
@@ -0,0 +1,11 @@
+package cat.cercavila.cvapi.configuration.restClient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfiguration {
+    @Bean
+    public WebClient createWebClient() { return WebClient.create("http://localhost/api"); }
+}
