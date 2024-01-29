d864bdce78e5f8347a4b893605663d8a23f1ff78
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-01-29 17:51:24 +0100

    cv-api | Adapter in web StoreCollaController REST added

---
+++ cv-api/src/main/java/cat/cercavila/cvapi/colles/adapter/in/web/StoreCollaController.java
@@ -0,0 +1,19 @@
+package cat.cercavila.cvapi.colles.adapter.in.web;
+
+import cat.cercavila.cvapi.colles.application.port.in.CreateColla;
+import cat.cercavila.cvapi.colles.application.port.in.CreateCollaCommand;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StoreCollaController {
+    private CreateColla createColla;
+
+    public StoreCollaController(CreateColla createColla) { this.createColla = createColla; }
+
+    @PostMapping("/colles")
+    public void storeColla(@RequestBody CreateCollaCommand createCollaCommand) {
+        createColla.createColla(createCollaCommand);
+    }
+}
