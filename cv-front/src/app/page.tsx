36e367f1e58d51a9becb374ef50e0662bc31e4ad
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-02-07 00:06:33 +0100

    cv-front | Linking page.tsx to colles.tsx

--- cv-front/src/app/page.tsx
+++ cv-front/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          En procés d&apos;edició de&nbsp;
+          FASE DE PROVES - En procés d&apos;edició de&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
         <p > Aquí comença la Cercavila!</p>
@@ -93,19 +93,19 @@ export default function Home() {
         </a>
 
         <a
-          href="http://www.gegantsmataro.net/festes.php?any=Actual"
+          href="/colles"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Rànquing{" "}
+            Colles a Cercavila{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Descobreix les figures més actives de Mataró.
+            Descobreix les colles registrades a Cercavila.
           </p>
         </a>
 
