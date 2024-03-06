39ea9b6561f69a74765477f0bbcdd6d3bae72143
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-03-06 14:04:19 +0100

    cv-front | Added test CreateCollaForm and added react and user-event libraries
    Still to test
    Not added mocked repository because of already having a real one.

--- cv-front/src/modules/colles/domain/colla-attributes/CollaEntity.ts
+++ cv-front/src/modules/colles/domain/colla-attributes/CollaEntity.ts
@@ -1,5 +1,6 @@
 export const ENTITY_MIN_LENGTH = 3;
 export const ENTITY_MAX_LENGTH = 120;
+export const ENTITY_ERROR_MESSAGE = `L'entitat no és vàlida. L'entitat ha de tenir entre ${ENTITY_MIN_LENGTH} i ${ENTITY_MAX_LENGTH} caràcters vàlids.`;
 
 export function isCollaEntityValid(entity: string): boolean {
     if (entity.length <= ENTITY_MIN_LENGTH || entity.length >= ENTITY_MAX_LENGTH+1) return false;
@@ -8,5 +9,5 @@ export function isCollaEntityValid(entity: string): boolean {
 }
 
 export function CollaEntityNotValidError(entity: string): Error {
-    return new Error(`L'entitat ${entity} no és vàlida. L'entitat ha de tenir entre ${ENTITY_MIN_LENGTH} i ${ENTITY_MAX_LENGTH} caràcters vàlids.`);
+    return new Error(ENTITY_ERROR_MESSAGE);
 }
--- cv-front/src/modules/colles/domain/colla-attributes/CollaName.ts
+++ cv-front/src/modules/colles/domain/colla-attributes/CollaName.ts
@@ -1,5 +1,6 @@
 export const NAME_MIN_LENGTH = 3;
 export const NAME_MAX_LENGTH = 120;
+export const NAME_ERROR_MESSAGE = `El nom no és vàlid. El nom ha de tenir entre ${NAME_MIN_LENGTH} i ${NAME_MAX_LENGTH} caràcters vàlids.`;
 
 export function isCollaNameValid(name: string): boolean {
     if (name.length <= NAME_MIN_LENGTH || name.length > NAME_MAX_LENGTH+1) return false;
@@ -8,5 +9,5 @@ export function isCollaNameValid(name: string): boolean {
 }
 
 export function CollaNameNotValidError(name: string): Error {
-    return new Error(`El nom ${name} no és vàlid. El nom ha de tenir entre ${NAME_MIN_LENGTH} i ${NAME_MAX_LENGTH} caràcters vàlids.`);
+    return new Error(NAME_ERROR_MESSAGE);
 }
