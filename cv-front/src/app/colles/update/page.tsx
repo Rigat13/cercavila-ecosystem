1f8fff61e8c9416c60f91d7b3c36c3eae897badd
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-04-15 23:46:36 +0200

    cv-front | Added sidebar to update and main page
    TODO 2.4: Mantain language settings across main page.
    TODO 3 (optional): Replace content.ts dictionary creation for csv sheet.

--- cv-front/src/app/colles/update/page.tsx
+++ cv-front/src/app/colles/update/page.tsx
@@ -6,24 +6,29 @@ import {UpdateCollaForm} from "@/app/sections/colles/update-form/UpdateCollaForm
 import {useSearchParams } from "next/navigation";
 import {defaultLang, dictionary} from "@/content";
 import styles from "@/app/sections/colles/list/CollesList.module.scss";
+import {useState} from "react";
+import SidebarMenu from "@/app/sections/shared/SidebarMenu";
+import stylesSidebar from "@/app/sections/shared/SidebarMenu.module.scss";
 
 export default function Page() {
     const searchParams = useSearchParams();
     const collaId = searchParams.get('collaId');
     const lang = searchParams.get('lang') || defaultLang;
-
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const toggleSidebar = () => { setIsSidebarOpen(!isSidebarOpen); };
     const repository = createApiCollaRepository();
 
     return (
         <CollesContextProvider repository={repository}>
-            <Image
-                src="/cercavila_logo.svg"
-                alt="Logotip de Cercavila"
-                className=""// dark:invert for dark logo on dark mode
-                width={100}
-                height={24}
-                priority
-            />
+            <div className="flex justify-center items-center">
+                <Image src="/cercavila_logo.svg" alt="Logotip de Cercavila" className="}" width={80} height={80} />
+            </div>
+            <div className={stylesSidebar.sidebar}>
+                <button className={stylesSidebar.sidebarButton} onClick={toggleSidebar}>
+                    <img src="/icons/logo-burger.svg" alt="Side bar" />
+                </button>
+                <SidebarMenu  isOpen={isSidebarOpen} onClose={toggleSidebar} lang={lang}/>
+            </div>
 
             <div className = "Colles">
                 <h1 className={styles.h1}>{dictionary[lang]?.cercavilaTitle}</h1>
