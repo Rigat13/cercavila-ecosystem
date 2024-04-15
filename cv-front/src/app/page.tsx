c848400d38d80ba0319f485977ee281b31861ca4
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-04-16 01:35:52 +0200

    cv-front | Made main page message bar not cover side bar button.

--- cv-front/src/app/page.tsx
+++ cv-front/src/app/page.tsx
@@ -31,7 +31,7 @@ function HomeContent() {
         <SidebarMenu  isOpen={isSidebarOpen} onClose={toggleSidebar} lang={lang}/>
       </div>
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="z-9 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             FASE DE PROVES - En procés d&apos;edició de&nbsp;
             <code className="font-mono font-bold">src/app/page.tsx</code>
