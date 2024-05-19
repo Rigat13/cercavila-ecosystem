519a88b46f4d1be9f6c4de5a944e166f8a292d67
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-05-19 08:36:59 +0200

    cv-front | User base card added roles

--- cv-front/src/app/sections/users/card/UserCard.tsx
+++ cv-front/src/app/sections/users/card/UserCard.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./UserCard.module.scss";
+import createUserStyles from "@/app/sections/users/form/UserForm.module.scss";
 import digiProductStyles from "@/app/sections/digitalProducts/card/DigitalProductCard.module.scss";
 import { User } from "@/modules/users/domain/User";
-import {defaultLang} from "@/content";
+import {defaultLang, dictionary} from "@/content";
 import {Figura} from "@/modules/figures/domain/Figura";
+import {getRolesAdditionalStyle} from "@/modules/users/domain/user-attributes/UserRoles";
+import {getContrastColour} from "@/app/sections/shared/getContrastColour";
+import {useUsersContext} from "@/app/sections/users/UsersContext";
 
 interface UserCardProps {
     user: User;
@@ -22,6 +26,7 @@ export function UserCard({ user, lang }: { user: User; lang: string }) {
         activeUserTitle,
         activeUserBackgroundColour,
     } = user;
+    const { colles } = useUsersContext();
 
     return (
         <div className={styles.userCard}>
@@ -34,6 +39,23 @@ export function UserCard({ user, lang }: { user: User; lang: string }) {
                 <h3 className={styles.userCard__nickname}>@{nickname}</h3>
                 <p className={styles.userCard__name}>{name}</p>
                 <p className={styles.userCard__surnames}>{firstSurname+" "+secondSurname}</p>
+
+
+                <div className={createUserStyles.selectedElements}>
+                    {user.roles.toString().split(',')
+                        .map((collaRole, index) => {
+                        const [roleName, collaId] = collaRole.split('-');
+                        const colla = colles.find((colla) => colla.id === collaId);
+                        return colla &&(
+                            <div key={index} className={createUserStyles.selectedElementCombined}>
+                                        <span className={createUserStyles.selectedRole} style={ getRolesAdditionalStyle(roleName) }>
+                                            {dictionary[lang]?.[roleName]} </span>
+                                <span className={createUserStyles.selectedColla} style={{ backgroundColor: colla.primaryColour, color: getContrastColour(colla.primaryColour) }}>
+                                            {colla?.name} </span>
+                            </div>
+                        );
+                    })}
+                </div>
                 {/* Include <p className={styles.userCard__roles}>Roles: {roles.join(", ")}</p>
             <p className={styles.userCard__digitalProducts}>
                 Digital Products: {digitalProducts.join(", ")}
