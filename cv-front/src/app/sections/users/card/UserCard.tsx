3f5821bb12dfc92ad578753ab0d29a315586f49f
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-05-19 07:30:20 +0200

    cv-front | User base card stylised: nickname and names (no email nor password)

--- cv-front/src/app/sections/users/card/UserCard.tsx
+++ cv-front/src/app/sections/users/card/UserCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./UserCard.module.scss";
+import digiProductStyles from "@/app/sections/digitalProducts/card/DigitalProductCard.module.scss";
 import { User } from "@/modules/users/domain/User";
 import {defaultLang} from "@/content";
 import {Figura} from "@/modules/figures/domain/Figura";
@@ -10,40 +11,35 @@ interface UserCardProps {
 
 export function UserCard({ user, lang }: { user: User; lang: string }) {
     const {
-        name,
         nickname,
+        name,
         firstSurname,
         secondSurname,
-        email,
-        coins,
         roles,
-        digitalProducts,
         activeUserImage,
         activeUserImageFrame,
         activeUserBackgroundImage,
         activeUserTitle,
         activeUserBackgroundColour,
-        activePins,
     } = user;
 
     return (
         <div className={styles.userCard}>
-            <a href={`/users/update.html?userId=${user.id}${lang === defaultLang ? '' : `&lang=${lang}`}`}>
-                <button className={styles.updateButton}>
-                    <img src="/icons/icon-edit.svg" alt="Editar" />
-                </button>
-            </a>
-            <h3 className={styles.userCard__name}>{name}</h3>
-            <p className={styles.userCard__nickname}>@{nickname}</p>
-            <p className={styles.userCard__email}>{email}</p>
-            <p className={styles.userCard__coins}>Coins: {coins}</p>
-            <p className={styles.userCard__firstSurname}>First Surname: {firstSurname}</p>
-            <p className={styles.userCard__secondSurname}>Second Surname: {secondSurname}</p>
-            {/* Include <p className={styles.userCard__roles}>Roles: {roles.join(", ")}</p>
+            <div className={styles.userCard__info}>
+                <a href={`/users/update.html?userId=${user.id}${lang === defaultLang ? '' : `&lang=${lang}`}`}>
+                    <button className={styles.updateButton}>
+                        <img src="/icons/icon-edit.svg" alt="Editar" />
+                    </button>
+                </a>
+                <h3 className={styles.userCard__nickname}>@{nickname}</h3>
+                <p className={styles.userCard__name}>{name}</p>
+                <p className={styles.userCard__surnames}>{firstSurname+" "+secondSurname}</p>
+                {/* Include <p className={styles.userCard__roles}>Roles: {roles.join(", ")}</p>
             <p className={styles.userCard__digitalProducts}>
                 Digital Products: {digitalProducts.join(", ")}
             </p>
             {/* Include other user details you want to display */}
+            </div>
         </div>
     );
 };
