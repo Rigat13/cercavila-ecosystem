f69a8bc62a6ab568499053aad4d95c44c0d7e67f
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-05-20 16:52:37 +0200

    cv-front | User Page left column structured

--- cv-front/src/app/sections/users/user/UserPage.tsx
+++ cv-front/src/app/sections/users/user/UserPage.tsx
@@ -83,71 +83,53 @@ export function UserPage({ user, lang }: { user: User; lang: string }) {
     const customTheme = activeUserBackgroundColour && theme ? {
         backgroundColor: theme.primaryColour,
         color: theme.secondaryColour,
-        boxShadow: isHovered ? ' 0 0 0rem 0.6rem ' + theme.secondaryColour : '',
         transition: 'background-color 0.3s ease, color 0.3s ease, box-shadow 0.3s ease',
     } : {};
 
     const isMobile = useMediaQuery('(max-width: 768px)');
     
     return (
-        <div className={styles.layout}>
+        <div className={styles.userPage} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={customTheme}>
             {isMobile ? (
                 <>
                     <div className={styles.row}>
-                        <div className={styles.component1}>Component 1</div>
-                        <div className={styles.column}>
-                            <div className={styles.component2}>Component 2</div>
-                            <div className={styles.component3}>Component 3</div>
-                        </div>
-                    </div>
-                    <div className={styles.component4}>Component 4</div>
-                    <div className={styles.component5}>Component 5</div>
-                </>
-            ) : (
-                <>
-                    <div className={styles.leftColumn}>
-                        <div className={styles.component1}>Component 1</div>
-                        <div className={styles.component4}>Component 4</div>
-                    </div>
-                    <div className={styles.rightColumn}>
-                        <div className={styles.topRow}>
-                            <div className={styles.component2}>Component 2</div>
-                            <div className={styles.component3}>Component 3</div>
-                        </div>
-                        <div className={styles.component5}>Component 5</div>
-                    </div>
-                </>
-            )}
-        </div>
-        /*<div className={styles.userPage}>
-            <div className={styles.userPage__info}  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={customTheme}>
-                <div className={styles.userPage__topLeftPanel}>
-                    <div className={styles.userPage__topLeftPanel__container}>
-                        <a target="_blank" className={classNames(detailsStyles.digitalProductDetails__aImage, styles.userPage__aImage)}
-                           href={lang === defaultLang ? `/users/user.html?userId=${user.id}` : `/users/user.html?userId=${user.id}&lang=${lang}`}>
-                            <div className={classNames(detailsStyles.digitalProductDetails__image, styles.userPage__image)}>
-                                <img
-                                    src={imageUrl || getDefaultUserImage()}
-                                    style={!imageUrl ? randomColourFilter : {}}
-                                />
-                            </div>
-                        </a>
-                        {imageFrameUrl && <a target="_blank" className={classNames(detailsStyles.digitalProductDetails__aImageFrame, styles.userPage__aImageFrame)}
-                                             href={lang === defaultLang ? `/users/user.html?userId=${user.id}` : `/users/user.html?userId=${user.id}&lang=${lang}`}>
-                            <div className={classNames(detailsStyles.digitalProductDetails__imageFrame, styles.userPage__imageFrame)}>
-                                <img
-                                    src={imageFrameUrl}
-                                />
-                            </div>
-                        </a>}
-                        <a target="_blank" className={classNames(detailsStyles.digitalProductDetails__aTitle, styles.userPage__aTitle)}>
-                            {title && <div className={classNames(detailsStyles.digitalProductDetails__title, styles.userPage__title)}
-                                           style={{ background: title.primaryColour, color: title.secondaryColour }}>
-                                {title.name}
-                                <div className={detailsStyles.digitalProductDetails__shine}></div>
-                            </div>}
-                        </a>
+                        <div className={styles.component1}>
+                            <a target="_blank" className={styles.userPage__aImage}
+                               href={lang === defaultLang ? `/users/user.html?userId=${user.id}` : `/users/user.html?userId=${user.id}&lang=${lang}`}>
+                                <div className={styles.userPage__image}>
+                                    <img
+                                        src={imageUrl || getDefaultUserImage()}
+                                        style={!imageUrl ? randomColourFilter : {}}
+                                    />
+                                </div>
+                                {imageFrameUrl &&
+                                    <a target="_blank" className={styles.userPage__aImageFrame}
+                                       href={lang === defaultLang ? `/users/user.html?userId=${user.id}` : `/users/user.html?userId=${user.id}&lang=${lang}`}>
+                                        <div className={styles.userPage__imageFrame}>
+                                            <img
+                                                src={imageFrameUrl}
+                                            />
+                                        </div>
+                                    </a>}
+                            </a>
 
+                            <a target="_blank" className={styles.userPage__aTitle}>
+                                {title &&
+                                    <div className={styles.userPage__title} style={{ background: title.primaryColour, color: title.secondaryColour }}>
+                                    {title.name}
+                                    <div className={detailsStyles.digitalProductDetails__shine}></div>
+                                </div>}
+                            </a>
+                        </div>
+                        <div className={styles.column}>
+                            <div className={styles.component2}>
+                                <div className={styles.userPage__nickname} style={customTheme}>@{nickname} </div>
+                                <p className={styles.userPage__names}>{name+" "+firstSurname+" "+secondSurname}</p>
+                            </div>
+                            <div className={styles.component3}>Component 3</div>
+                        </div>
+                    </div>
+                    <div className={styles.component4}>
                         <div className={styles.selectedElements}>
                             {sortedRoles.toString().split(',')
                                 .map((collaRole, index) => {
@@ -164,12 +146,83 @@ export function UserPage({ user, lang }: { user: User; lang: string }) {
                                 })}
                         </div>
                     </div>
+                    <div className={styles.component5}>Component 5</div>
+                </>
+            ) : (
+                <>
+                    <div className={styles.leftColumn}>
+                        <div className={styles.component1}>
+                            <a target="_blank" className={styles.userPage__aImage}
+                               href={lang === defaultLang ? `/users/user.html?userId=${user.id}` : `/users/user.html?userId=${user.id}&lang=${lang}`}>
+                                <div className={styles.userPage__image}>
+                                    <img
+                                        src={imageUrl || getDefaultUserImage()}
+                                        style={!imageUrl ? randomColourFilter : {}}
+                                    />
+                                </div>
+                                {imageFrameUrl &&
+                                    <a target="_blank" className={styles.userPage__aImageFrame}
+                                       href={lang === defaultLang ? `/users/user.html?userId=${user.id}` : `/users/user.html?userId=${user.id}&lang=${lang}`}>
+                                        <div className={styles.userPage__imageFrame}>
+                                            <img
+                                                src={imageFrameUrl}
+                                            />
+                                        </div>
+                                    </a>}
+                            </a>
+
+                            <a target="_blank" className={styles.userPage__aTitle}>
+                                {title &&
+                                    <div className={styles.userPage__title} style={{ background: title.primaryColour, color: title.secondaryColour }}>
+                                        {title.name}
+                                        <div className={detailsStyles.digitalProductDetails__shine}></div>
+                                    </div>}
+                            </a>
+                        </div>
+                        <div className={styles.component4}>
+                            <div className={styles.selectedElements}>
+                                {sortedRoles.toString().split(',')
+                                    .map((collaRole, index) => {
+                                        const [roleName, collaId] = collaRole.split('-');
+                                        const colla = colles.find((colla) => colla.id === collaId);
+                                        return colla &&(
+                                            <div key={index} className={styles.selectedElementCombined}>
+                                        <span className={styles.selectedRole} style={ getRolesAdditionalStyle(roleName) }>
+                                            {dictionary[lang]?.[roleName]} </span>
+                                                <span className={styles.selectedColla} style={{ backgroundColor: colla.primaryColour, color: getContrastColour(colla.primaryColour) }}>
+                                            {colla?.name} </span>
+                                            </div>
+                                        );
+                                    })}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.rightColumn}>
+                        <div className={styles.topRow}>
+                            <div className={styles.component2}>
+                                <div className={styles.userPage__nickname} style={customTheme}>@{nickname} </div>
+                                <p className={styles.userPage__names}>{name+" "+firstSurname+" "+secondSurname}</p>
+                            </div>
+                            <div className={styles.component3}>Component 3</div>
+                        </div>
+                        <div className={styles.component5}>Component 5</div>
+                    </div>
+                </>
+            )}
+        </div>
+        /*<div className={styles.userPage}>
+            <div className={styles.userPage__info}  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={customTheme}>
+                <div className={styles.userPage__topLeftPanel}>
+                    <div className={styles.userPage__topLeftPanel__container}>
+
+
+
+                    </div>
                 </div>
 
                 <div className={styles.userPage__topRightPanel}>
                     <div className={styles.userPage__topRightPanel__container}>
-                        <div className={styles.userPage__nickname} style={customTheme}>@{nickname} </div>
-                        <p className={styles.userPage__names}>{name+" "+firstSurname+" "+secondSurname}</p>
+
                     </div>
                 </div>
 
