dd997b9b90b4e69424330952f8106a50c512f569
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-05-23 10:21:54 +0200

    cv-front | Login logout button added

--- cv-front/src/app/login/page.tsx
+++ cv-front/src/app/login/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 import Image from "next/image";
-import {createApiUserRepository} from "@/modules/users/infrastructure/ApiUserRepository";
-import {UsersContextProvider, useUsersContext} from "@/app/sections/users/UsersContext";
-import {useSearchParams } from "next/navigation";
-import {defaultLang, dictionary} from "@/content";
-import {useEffect, useState} from "react";
+import { createApiUserRepository } from "@/modules/users/infrastructure/ApiUserRepository";
+import { UsersContextProvider, useUsersContext } from "@/app/sections/users/UsersContext";
+import { useSearchParams } from "next/navigation";
+import { defaultLang, dictionary } from "@/content";
+import { useEffect, useState } from "react";
 import SidebarMenu from "@/app/sections/shared/SidebarMenu";
 import stylesSidebar from "@/app/sections/shared/SidebarMenu.module.scss";
-import {Suspense} from "react";
-import {UserPageHolder} from "@/app/sections/users/user/UserPageHolder";
+import { Suspense } from "react";
+import { UserPageHolder } from "@/app/sections/users/user/UserPageHolder";
 import UserProfile from "@/app/sections/login/UserProfile";
 import Login from "@/app/sections/login/Login";
 
@@ -30,6 +30,7 @@ function PageContent() {
     const repository = createApiUserRepository();
 
     const [token, setToken] = useState<string | null>(null);
+
     const handleLogin = (token: string) => {
         console.log("Logging in with token:", token);
         setToken(token);
@@ -38,14 +39,30 @@ function PageContent() {
         console.log(localStorage.getItem('token'));
     };
 
+    const handleLogout = () => {
+        setToken(null);
+        localStorage.removeItem('token');
+    };
 
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        if (storedToken) {
+            setToken(storedToken);
+        }
+    }, []);
 
     return (
-            <Suspense fallback={<div>Loading...</div>}>
-
-                <div>
-                    {token ? <p> Success </p> : <Login onLogin={handleLogin} />}
-                </div>
-            </Suspense>
-    )
+        <Suspense fallback={<div>Loading...</div>}>
+            <div>
+                {token ? (
+                    <div>
+                        <p>Success. Token: {token}</p>
+                        <button onClick={handleLogout}>Logout</button>
+                    </div>
+                ) : (
+                    <Login onLogin={handleLogin} />
+                )}
+            </div>
+        </Suspense>
+    );
 }
