bcd46fdd85ccc36042f43e5cfd45e7516c2d0a3d
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-06-24 03:21:30 +0200

    cv-front | Added HTTPS support for cv-api connection.

--- cv-front/src/mainconfiguration.ts
+++ cv-front/src/mainconfiguration.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
-export const GLOBAL_URL_PREFIX = dev ? "http://81.25.126.202:8080" : "http://81.25.126.202:8080";
+export const GLOBAL_URL_PREFIX = dev ? "https://cl2024021422001.dnssw.net" : "https://cl2024021422001.dnssw.net";
 export const ADMIN = "TODO"; // TODO
