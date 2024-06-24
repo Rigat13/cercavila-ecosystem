6902ae314fd599eeb33cf7c2b4a0b1ffe62f1128
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-06-24 03:01:11 +0200

    cv-front | Infrastructures configuration URL centralized to a main configuration for simpler modification
    Also made https://cl2024021422001.dnssw.net/api/colles to work from nginx Ubuntu Server.

---
+++ cv-front/src/mainconfiguration.ts
@@ -0,0 +1,3 @@
+const dev = process.env.NODE_ENV !== "production";
+export const GLOBAL_URL_PREFIX = dev ? "http://81.25.126.202:8080" : "http://81.25.126.202:8080";
+export const ADMIN = "TODO"; // TODO
